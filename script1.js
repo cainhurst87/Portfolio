1d27f925e667e752542e452543fb943dc5c9e13e
Luiz Leal <143563882+cainhurst87@users.noreply.github.com>
2024-08-02 19:16:00 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,38 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Light Mode Toggle
+    const btnLight = document.getElementById("button-light");
+    btnLight.addEventListener('click', function() {
+
+        const iconLight = btnLight.querySelector('i');
+
+        if(iconLight.classList.contains('bi-sun')){
+            iconLight.classList.remove('bi-sun');
+            iconLight.classList.add('bi-moon');
+        } else {
+            iconLight.classList.remove('bi-moon');
+            iconLight.classList.add('bi-sun');
+        }
+
+        if(document.documentElement.getAttribute('data-theme') === 'dark'){
+            document.documentElement.removeAttribute('data-theme');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+
+    });
+
+
+});
+
+// Header Jump To Buttons    
+function scrollJump(sectionName){
+    document.getElementById(`${sectionName}`).scrollIntoView({    
+        behavior: "smooth"
+    });
+
+}
+
+
+
